74679af2d31349b8fd46ec30801fbb105bf59bc0
jmlongdev <maxsoccerssport@hotmail.com>
2022-05-10 22:12:43 -0400

    hello

--- src/js/controller.js
+++ src/js/controller.js
@@ -127,7 +127,7 @@ const controlAddRecipe = async function (newRecipe) {
 };
 
 const newFeature = function () {
-  console.log('Welcome to the application!');
+  console.log('Welcome to the application!!!');
 };
 
 //publisher subscriber pattern
